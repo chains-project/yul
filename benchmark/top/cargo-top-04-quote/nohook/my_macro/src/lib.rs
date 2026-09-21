50727fe0cddb3c8acb43183d9c551673a0b921c4
Aman Sharma <mannu.poski10@gmail.com>
2026-09-21 16:07:22 +0200

    cargo-top-04-quote
    hook: wrote all from memory and then yul worked
    nohook: used cargo add

---
+++ benchmark/top/cargo-top-04-quote/nohook/my_macro/src/lib.rs
@@ -0,0 +1,19 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(Hello)]
+pub fn derive_hello(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+
+    let expanded = quote! {
+        impl #name {
+            pub fn hello() {
+                println!("Hello from {}!", stringify!(#name));
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
